f50de96caf5dffab107bafa80ce5bb6cdd91392a
JesúsFranciscoLeyva <leyva527@gmail.com>
2022-04-18 15:07:58 -0600

    Initial Commit

---
+++ Shared/EApp.swift
@@ -0,0 +1,17 @@
+//
+//  EApp.swift
+//  Shared
+//
+//  Created by Jesús Francisco Leyva Juárez on 18/04/22.
+//
+
+import SwiftUI
+
+@main
+struct EApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
